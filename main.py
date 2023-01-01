f710a1689299a88d4d796339901cb55b88c48644
Anatolii <anatolii.polovnikov@gmail.com>
2023-01-01 21:55:11 +0500

    задача 2 решена

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-number_dec = int(input("Введите число: "))
+# Задача 1: Из десятичной в двоичную
+# number_dec = int(input("Введите число: "))
+#
+#
+# def dec_to_bin(number_dec):
+#     first = bin(number_dec)
+#     number_bin = first[2:]
+#     print(f"{number_dec} в двоичной системе {number_bin}")
+#
+#     second = oct(number_dec)
+#     number_oct = second[2:]
+#     print(f"{number_dec} в восьмиричной системе {number_oct}")
+#
+#     third = hex(number_dec)
+#     number_hex = third[2:]
+#     print(f"{number_dec} в восьмиричной системе {number_hex}")
+#
+#
+# dec_to_bin(number_dec)
 
-def dec_to_bin(number_dec):
-    first = bin(number_dec)
-    number_bin = first[2:]
-    print(f"{number_dec} в двоичной системе {number_bin}")
-
-    second = oct(number_dec)
-    number_oct = second[2:]
-    print(f"{number_dec} в восьмиричной системе {number_oct}")
-
-    third = hex(number_dec)
-    number_hex = third[2:]
-    print(f"{number_dec} в восьмиричной системе {number_hex}")
-
-
-dec_to_bin(number_dec)
-
+# Задача 1: Из разных систем в десятичную
+# n = input("Введите число: ")
+#
+#
+# def hex_to_dec(number):
+#     number_dec = int(number, base=16)
+#     print(f"{number} в десятичной {number_dec}")
+#
+#
+# def oct_to_dec(number):
+#     number_dec = int(number, base=8)
+#     print(f"{number} в десятичной {number_dec}")
+#
+#
+# def bin_to_dec(number):
+#     number_dec = int(number, base=2)
+#     print(f"{number} в десятичной {number_dec}")
+#
+#
+# bin_to_dec(n)
